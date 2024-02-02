611a351bf638c13d595ec9fbaa0677b857204431
nilgordo <150517639+nilgordo@users.noreply.github.com>
2024-02-02 13:48:17 +0100

    Update Bateria1.1.py

--- Bateria1.1.py
+++ Bateria1.1.py
@@ -1,1 +1,6 @@
-· Programa que pregunta dos números a l'usuari i calcula la seva suma.
+# Programa que pregunta dos números a l'usuari i calcula la seva suma.
+num1 = input("Introdueix un numero: ")
+num2 = input("Intodueix l'altre numero: ")
+resultat = int(num1) + int(num2)
+
+print("El resultat de la suma és: ", resultat)
