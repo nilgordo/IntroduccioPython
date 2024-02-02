0136fab28e6bbc15d5e689a82e62756489e64745
nilgordo <150517639+nilgordo@users.noreply.github.com>
2024-02-02 13:59:48 +0100

    Create Bateria1.5.py

---
+++ Bateria1.5.py
@@ -0,0 +1,7 @@
+# Programa que demana una temperatura en graus Celsius a l'usuari i retorna la temperatura en graus Fahrenheit.
+
+graus = input("Introdueix els ºC: ")
+resultat_fahrenheit = (float(graus)*1.8)+32
+
+
+print("Son",resultat_fahrenheit,"ªF")
